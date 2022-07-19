1adb2b959c681b73ede8fb59437053e82ba6c7d5
matisagardia <matisagardia7@gmail.com>
2022-07-19 14:02:30 -0300

    Round and score counters added

--- javascript.js
+++ javascript.js
@@ -1,42 +1,73 @@
-function playRound(getUserChoice, getComputerChoice) {
-    function getUserChoice(choice) {
-      if (choice === 'rock' || choice === 'paper' || choice === 'scissors') {
-        return choice;
-      } else {
-        return 'Wrong input';
+
+let userCount = 0;
+let computerCount = 0;
+
+function game(){
+  
+
+    for (let index = 1; index < 6; index++) {
+
+      console.log('Round' + ' ' + index + '/' + '5');
+
+    function playRound() {
+
+      function getUserChoice(choice) {
+        if (choice === 'rock' || choice === 'paper' || choice === 'scissors') {
+          return choice;
+        } else {
+          return 'Wrong input';
+        }
       }
-    }
-  
-    function getComputerChoice() {
-      let number = Math.floor(Math.random() * 3);
-      if (number === 0) {
-        return 'rock';
-      } else if (number === 1) {
-        return 'paper';
-      } else {
-        return 'scissors';
+
+      function getComputerChoice() {
+        let number = Math.floor(Math.random() * 3);
+        if (number === 0) {
+          return 'rock';
+        } else if (number === 1) {
+          return 'paper';
+        } else {
+          return 'scissors';
+        }
       }
+      const userChoice = getUserChoice(prompt('Rock, paper or scissors?'));
+      const computerChoice = getComputerChoice();
+      let userWin = 'You won! Paper beats rock!';
+      let computerWin = 'You lost! Rock beats scissors!';
+
+    
+        console.log('The user choice was: ' + userChoice);
+        console.log('The computer choice was: ' + computerChoice);
+
+      function match() {
+      if (userChoice === computerChoice) {
+        return 'Tied!';
+      } else if (userChoice === 'rock' && computerChoice === 'paper') {
+        return computerWin;
+      } else if (userChoice === 'rock' && computerChoice === 'scissors') {
+        return userWin;
+      } else if (userChoice === 'paper' && computerChoice === 'rock') {
+        return userWin;
+      } else if (userChoice === 'paper' && computerChoice === 'scissors') {
+        return computerWin;
+      } else if (userChoice === 'scissors' && computerChoice === 'rock') {
+        return computerWin;
+      } else {return userWin; }
     }
-  
-    const userChoice = getUserChoice(prompt('Rock, paper or scissors?'));
-    const computerChoice = getComputerChoice();
-  
-      console.log('The user choice was: ' + userChoice);
-      console.log('The computer choice was: ' + computerChoice);
-  
-    if (userChoice === computerChoice) {
-      return 'Tied!';
-    } else if (userChoice === 'rock' && computerChoice === 'paper') {
-      return 'You lost! Paper beats rock!';
-    } else if (userChoice === 'rock' && computerChoice === 'scissors') {
-      return 'You won! Rock beats scissors!';
-    } else if (userChoice === 'paper' && computerChoice === 'rock') {
-      return 'You won! Paper beats rock!';
-    } else if (userChoice === 'paper' && computerChoice === 'scissors') {
-      return 'You lost! Scissors beats paper!';
-    } else if (userChoice === 'scissors' && computerChoice === 'rock') {
-      return 'You lost! Rock beats scissors!';
-    } else return 'You won! Scissors beats paper!';
+    console.log(match());
+
+    if(match() == computerWin){
+      computerCount++;
+    } else if (match() == userWin) {
+      userCount++;
+    }
+    console.log('User points:' + ' ' + userCount);
+    console.log('Computer points:' + ' ' + computerCount);
+
+    }
+
+    console.log(playRound())
   }
-  
-  console.log(playRound());
+}
+
+    console.log(game());
+
